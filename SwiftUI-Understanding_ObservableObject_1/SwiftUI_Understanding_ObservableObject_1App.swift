7d094c15d737ea3f6fad494cf262f2c2622b7dd2
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 20:56:08 +0530

    Initial Commit

---
+++ SwiftUI-Understanding_ObservableObject_1/SwiftUI_Understanding_ObservableObject_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Understanding_ObservableObject_1App.swift
+//  SwiftUI-Understanding_ObservableObject_1
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Understanding_ObservableObject_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-Understanding_ObservableObject_1Tests/SwiftUI_Understanding_ObservableObject_1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftUI_Understanding_ObservableObject_1Tests.swift
+//  SwiftUI-Understanding_ObservableObject_1Tests
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import XCTest
+@testable import SwiftUI_Understanding_ObservableObject_1
+
+class SwiftUI_Understanding_ObservableObject_1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
